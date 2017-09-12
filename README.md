75773262da0549d08f73db265f069ee9195c597a
Jerry Yang <zhenyoung@gmail.com>
2017-09-12 11:06:20 -0700

    Add init.sh for new mac startup

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 Just some scripts I use regularly.  
 
 This is primarily used on Mac, but should also work for linux in general.
+
+```sh
+sh -c "$(wget -qO- https://raw.githubusercontent.com/islanderman/misc/master/init.sh)"
+```
+
---
+++ init.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+PKGS=(aria2 autoconf automake bash bash-completion boost brew-cask-completion cairo cmake cpanminus curl faac fdk-aac ffmpeg fontconfig freetype fribidi fzf gcc gdbm gettext git glib gmp go gobject-introspection graphite2 harfbuzz highlight icu4c isl joe lame libass libav libev libffi libmpc libogg libpng libssh2 libtool libvorbis libvpx libyaml lua maven midnight-commander mpfr mplayer mysql nmap node openssl opus parallel pcre pixman pkg-config protobuf python python3 qt readline rtmpdump s-lang sqlite ssh-copy-id texi2html thefuck theora tomcat tree unrar vim wakeonlan wget x264 xvid xz yasm youtube-dl yubico-piv-tool)
+TAPS=(buo/cask-upgrade caskroom/cask caskroom/fonts caskroom/versions domt4/autoupdate homebrew/core)
+CASK=(java caskroom/versions/google-chrome-beta caskroom/versions/iterm2-beta eclipse-java etcher font-fontawesome font-source-code-pro kkbox quicklook-csv quicklook-json sequel-pro sourcetree visual-studio-code welly)
+
+SEGMENT=7
+
+install() {
+  param1=("${!1}")
+
+  for (( i=0; i<${#param1[@]} ; i+=$3 )) ; do
+	    PKG=""
+		for (( j=0; j<$3 ; j++ )) ; do
+	    	PKG="$PKG ${param1[i+j]}"
+		done
+		brew $2 $PKG
+  done
+}
+
+# brew related
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+install TAPS[@] tap 1
+install PKGS[@] install $SEGMENT
+install CASK[@] cask install $SEGMENT
+
+brew cask cleanup
+brew cleanup
+
+# xcode
+xcode-select --install
+
+# space-vim
+sh -c "$(wget -qO- https://raw.githubusercontent.com/liuchengxu/space-vim/master/install.sh)" 
+
+wget https://raw.githubusercontent.com/islanderman/misc/master/.bashrc -O ~/.bash
+echo "source ~/.bash" >> ~/.bashrc
