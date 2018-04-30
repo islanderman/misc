9328d3bc842ba5452ea47563ea007fff29fec942
Jerry Yang <zhenyoung@gmail.com>
2018-04-30 10:14:24 -0700

    Update init.sh with its packages

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-PKGS=(aria2 autoconf automake bash bash-completion boost brew-cask-completion cairo cmake cpanminus curl faac fdk-aac ffmpeg fontconfig freetype fribidi fzf gcc gdbm gettext git glib gmp go gobject-introspection graphite2 harfbuzz highlight icu4c isl joe lame libass libav libev libffi libmpc libogg libpng libssh2 libtool libvorbis libvpx libyaml lua maven midnight-commander mpfr mplayer mysql nmap node openssl opus parallel pcre pixman pkg-config protobuf python python3 qt readline rtmpdump s-lang sqlite ssh-copy-id texi2html thefuck theora tomcat tree unrar vim wakeonlan wget x264 xvid xz yasm youtube-dl yubico-piv-tool)
+PKGS=(aria2 autoconf automake bash bash-completion boost brew-cask-completion cairo cmake cpanminus curl faac fdk-aac ffmpeg fontconfig freetype fribidi fzf gcc gdbm gettext git glib gmp go gobject-introspection google-java-format graphite2 harfbuzz highlight icu4c isl joe lame libass libav libev libffi libmpc libogg libpng libssh2 libtool libvorbis libvpx libyaml lua maven midnight-commander mpfr mplayer mysql nmap node openssl opus parallel pcre pixman pkg-config protobuf python python3 qt readline rtmpdump s-lang sqlite ssh-copy-id texi2html thefuck theora tomcat tree unrar vim wakeonlan wget x264 xvid xz yasm youtube-dl yubico-piv-tool)
 TAPS=(buo/cask-upgrade caskroom/cask caskroom/fonts caskroom/versions domt4/autoupdate homebrew/core)
-CASK=(java caskroom/versions/google-chrome-beta caskroom/versions/iterm2-beta eclipse-java etcher font-fontawesome font-source-code-pro kkbox quicklook-csv quicklook-json sequel-pro sourcetree visual-studio-code welly)
+CASK=(java java8 robo-3t atom docker cacher caskroom/versions/google-chrome-beta caskroom/versions/iterm2-beta eclipse-java etcher font-fontawesome font-source-code-pro quicklook-csv quicklook-json sequel-pro sourcetree visual-studio-code welly)
 
 SEGMENT=7
 
---
+++ script/.bash_profile
@@ -0,0 +1,10 @@
+if [ -f ~/.bashrc ]; then
+   source ~/.bashrc
+fi
+
+#export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_25.jdk/Contents/Home/"
+export CATALINA_HOME=/Library/Tomcat/Home
+
+
+PATH=/usr/local/sbin:$PATH
+
